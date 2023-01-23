f02d8649828fe29e58832758df4e839f16bdbe15
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2023-01-23 16:35:59 -0300

    Drop files from .gitignore

--- Pods/Target Support Files/Pods-TesteIOSDeveloperDigio-TesteIOSDeveloperDigioUITests/Pods-TesteIOSDeveloperDigio-TesteIOSDeveloperDigioUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TesteIOSDeveloperDigio_TesteIOSDeveloperDigioUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TesteIOSDeveloperDigio_TesteIOSDeveloperDigioUITests
-@end
